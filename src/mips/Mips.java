7bd3b8719e5dc9be0a30cdd7ceaa1714bc7ceb81
Bruno O <hilt@outlook.com.br>
2017-01-17 12:06:18 -0200

    First commit.

---
+++ src/mips/Mips.java
@@ -0,0 +1,19 @@
+/*
+ * This is project to show how to mips work
+ */
+package mips;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Mips {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
